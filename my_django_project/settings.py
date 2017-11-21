d355c46811020d73aebc57198911fab7b00eadfe
nrliangxy <nrliangxy@foxmail.com>
2017-11-21 18:46:34 +0800

    [add]

--- my_django_project/settings.py
+++ my_django_project/settings.py
@@ -75,14 +75,23 @@ WSGI_APPLICATION = 'my_django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME':'register',
+    'USER': 'root',
+    'PASSWORD': '2013814',
+    'HOST': 'localhost',
+    'PORT': '3306',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
